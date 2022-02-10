b1407fd742e48dc81c7ced957cd5c8e21c596bb3
RihardsZ77 <zelcsrihards7@gmail.com>
2022-02-10 15:05:27 +0200

    stundas darbs

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 alert(1);
+const vardi = ['Žanis Tārgalis',  'Silvestrs Stallone', "Arnolds Žangēvels", "Žans Vandamme", "Džikijs Čans" ];
+const balvas = ["Mobilais", "pica", "Nokia", "rācija", "binoklis"];
+const naudaKopa = 100000;//kopeja summa
+let uzvaretajuSkaits = 5;
+let rindas = document.querySelector(".rindas");
+rindas.innertHTML = "";
+for (let i = 0; i < uzvaretajuSkaits; i++){
+    let rand = Math.random() * vardi.length;
+    rand = Math.floor(rand);//noapalo uz leju
+
+    let uzvaretajs = vardi[rand];//izvada konsloe
+    let laimesti = balvas[rand];
+
+    rindas.innerHTML+=`
+
+    <tr>
+    <td>${i+1}</td>
+    <td>${uzvaretajs}</td>
+    </tr>`
+}
+
